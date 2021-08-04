e1c601b5aba62e97cd9daf56c3f9912b9b5d6803
Puksrevolution <puksbusinessworld@gmail.com>
2021-08-04 20:30:34 +0000

    Dev: Created and styled footer in base.html

--- static/js/base.js
+++ static/js/base.js
@@ -4,3 +4,6 @@ $(document).ready(function () {
     var path = scriptElement.getAttribute('data-path');
     $('a[href="'+path+'"]').addClass("active");
 });
+
+// update the year of the copyright automatically
+document.querySelector('#copyright-year').innerText = new Date().getFullYear();
